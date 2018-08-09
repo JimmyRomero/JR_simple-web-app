388336ed8c39033f45337ea8779160a5a62543f5
Jimmy Romero <jimmy.romero@fundacion-jala.org>
2018-08-09 18:58:36 -0400

    re-structure

---
+++ src/test/java/com/lanihuang/simplewebapp/beans/ProductTest.java
@@ -0,0 +1,13 @@
+package com.lanihuang.simplewebapp.beans;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ProductTest {
+
+@Test
+public void canConstructAPersonWithAName() {
+    assertEquals(true, true);
+    }
+}
